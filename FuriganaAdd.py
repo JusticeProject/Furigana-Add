ce7e77b49ed17e09a80a3fb4e2d77bfbe1d33943
JusticeProject <93150828+JusticeProject@users.noreply.github.com>
2022-07-26 12:33:59 -0500

    Add files via upload

---
+++ FuriganaAdd.py
@@ -0,0 +1,40 @@
+from urllib.request import urlopen
+from urllib.parse import quote
+from bs4 import BeautifulSoup
+import time
+import random
+
+###################################################################################################
+
+def get_furigana(kanji):
+    base_url = "https://jisho.org/search/"
+    url_parsed = base_url + quote(kanji)
+    html = urlopen(url_parsed)
+    bs = BeautifulSoup(html, "html.parser")
+
+    # get the furigana for the first result
+    span = bs.find("span", {"class": "furigana"})
+    if (span is None):
+        return ""
+    
+    spans = span.find_all("span")
+    hiragana_list = [item.text for item in spans]
+    hiragana = "".join(hiragana_list)
+    print("found " + hiragana + " for " + kanji)
+    return hiragana
+
+###################################################################################################
+
+words = open("words.txt", "r", encoding="utf-8").readlines()
+passage = open("passage.txt", "r", encoding="utf-8").read()
+
+for kanji in words:
+    furigana = get_furigana(kanji.strip())
+    ruby = "<ruby>" + kanji + "<rt>" + furigana + "</rt></ruby"
+    passage = passage.replace(kanji, ruby)
+    time.sleep(random.randint(30, 90))
+
+output = open("updated_passage.html", "w", encoding="utf-8")
+output.write(passage)
+output.close()
+print("done!")
